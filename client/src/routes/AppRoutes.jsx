b5df50e2448046eab2ac6cd1ff6bbc29bb4196b9
Tusharth Rangoliya <rangoliyatusharth@gmail.com>
2026-05-01 13:57:58 +0530

    fix: reorganize import statements and add route comments for better clarity

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,26 +1,32 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+// Auth
 import PublicPage from "../pages/auth/PublicPage.jsx";
 import AuthLayout from "../layouts/AuthLayout.jsx";
 import Login from "../pages/auth/Login.jsx";
 import Register from "../pages/auth/Register.jsx";
 
+// User
 import MainLayout from "../layouts/MainLayout.jsx";
 import Home from "../pages/user/Home.jsx";
 import AdDetails from "../pages/user/AdDetails.jsx";
-
-import DashboardLayout from "../layouts/DashboardLayout.jsx";
 import PublisherProfile from "../pages/publisher/PublisherProfile.jsx";
+import Profile from "../components/layout/Profile.jsx";
+
+// Admin
+import DashboardLayout from "../layouts/DashboardLayout.jsx";
 import Admin from "../pages/admin/AdminPage.jsx";
 import AllUserList from "../pages/admin/AllUserList.jsx";
 import AdminAds from "../pages/admin/AdminAds.jsx";
 
+// Publisher
 import PublisherPage from "../pages/publisher/PublisherPage.jsx";
 import MyAds from "../pages/publisher/MyAds.jsx";
 import CreateAd from "../pages/publisher/CreateAd.jsx";
 import Analytics from "../pages/publisher/Analytics.jsx";
 import Campaigns from "../pages/publisher/Campaigns.jsx";
-import Profile from "../components/layout/Profile.jsx";
+
+// Routes
 import ProtectedRoute from "./ProtectedRoute.jsx";
 import RoleRoute from "./RoleRoute.jsx";
 import { ROLES } from "../context/roles.js";
@@ -30,6 +36,8 @@ const router = createBrowserRouter([
     path: "/",
     element: <PublicPage />,
   },
+
+  // Auth Routes
   {
     element: <AuthLayout />,
     children: [
@@ -37,9 +45,12 @@ const router = createBrowserRouter([
       { path: "/register", element: <Register /> },
     ],
   },
+
+  // Protected Routes
   {
     element: <ProtectedRoute />,
     children: [
+      // USER ROUTES
       {
         element: <RoleRoute allowedRoles={[ROLES.USER]} />,
         children: [
@@ -54,6 +65,8 @@ const router = createBrowserRouter([
           },
         ],
       },
+
+      // ADMIN ROUTES
       {
         element: <RoleRoute allowedRoles={[ROLES.ADMIN]} />,
         children: [
@@ -67,6 +80,8 @@ const router = createBrowserRouter([
           },
         ],
       },
+
+      //PUBLISHER ROUTES
       {
         element: <RoleRoute allowedRoles={[ROLES.PUBLISHER]} />,
         children: [
